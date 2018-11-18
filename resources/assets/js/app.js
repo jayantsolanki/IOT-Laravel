552460c6b5e5741cdc1f7d93c93fcf88736265c0
Jayant Solanki <jayantjnp@gmail.com>
2018-11-18 11:16:49 -0500

    added paper ui

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,22 +3,22 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import App from './views/App'
-import Welcome from './views/Welcome'
+import App from "./App";
+import router from "./router/index";
+import PaperDashboard from "./plugins/paperDashboard";
+import "vue-notifyjs/themes/default.css";
 
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Welcome
-        },
-    ],
-});
+Vue.use(PaperDashboard);
+// import App from './views/App'
+// import Welcome from './views/Welcome'
 
-const app = new Vue({
-    el: '#app',
-    components: { App },
+/* eslint-disable no-new */
+new Vue({
     router,
-});
+    render: h => h(App)
+  }).$mount("#app");
+  
+
+
+
+
