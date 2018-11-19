bb5d6ca29ac9b0df26acb8d433a894391b0bd2f9
Jayant Solanki <jayantjnp@gmail.com>
2018-11-18 23:29:35 -0500

    modified css asset

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
        .js('resources/assets/js/bootstrap.js', 'public/js')
-       .sass('resources/assets/sass/app.scss', 'public/css');   
+       .sass('resources/assets/sass/app.scss', 'public/css') 
+       .sass('resources/assets/js/assets/sass/paper-dahsboard.scss', 'public/css');   
