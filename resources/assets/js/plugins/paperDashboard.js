8391f2273a7d2f69907324e5e669cafcda1a02e7
Jayant Solanki <jayantjnp@gmail.com>
2018-11-18 23:37:39 -0500

    modified path in plugin

--- resources/assets/js/plugins/paperDashboard.js
+++ resources/assets/js/plugins/paperDashboard.js
@@ -1,13 +1,13 @@
 import Notify from "vue-notifyjs";
-import SideBar from "@/components/SidebarPlugin";
+import SideBar from "../components/SidebarPlugin";
 import GlobalComponents from "./globalComponents";
 import GlobalDirectives from "./globalDirectives";
 import "es6-promise/auto";
 
 //css assets
 import "bootstrap/dist/css/bootstrap.css";
-import "@/assets/sass/paper-dashboard.scss";
-import "@/assets/css/themify-icons.css";
+import "../assets/sass/paper-dashboard.scss";
+import "../assets/css/themify-icons.css";
 
 export default {
   install(Vue) {
