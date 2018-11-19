c179c8e1def7b680fbb3a4fbc3117a6b65ac1bac
Jayant Solanki <jayantjnp@gmail.com>
2018-11-19 01:21:13 -0500

    removed images

--- resources/assets/js/pages/UserProfile/UserCard.vue
+++ resources/assets/js/pages/UserProfile/UserCard.vue
@@ -1,11 +1,11 @@
 <template>
   <card class="card-user">
     <div slot="image">
-      <img src="@/assets/img/background.jpg" alt="...">
+      <!-- <img src="@/assets/img/background.jpg" alt="..."> -->
     </div>
     <div>
       <div class="author">
-        <img class="avatar border-white" src="@/assets/img/faces/face-2.jpg" alt="...">
+        <!-- <img class="avatar border-white" src="@/assets/img/faces/face-2.jpg" alt="..."> -->
         <h4 class="title">Chet Faker
           <br>
           <a href="#">
