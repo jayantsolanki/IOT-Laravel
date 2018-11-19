c015615279d018015c3fb14befe2a306d8b6245e
Jayant Solanki <jayantjnp@gmail.com>
2018-11-18 23:25:49 -0500

    redefiend paths for the pages

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
 import App from "./App";
 import router from "./router/index";
+
 import PaperDashboard from "./plugins/paperDashboard";
 import "vue-notifyjs/themes/default.css";
 
 Vue.use(PaperDashboard);
-// import App from './views/App'
-// import Welcome from './views/Welcome'
 
 /* eslint-disable no-new */
 new Vue({
--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -1,15 +1,15 @@
-import DashboardLayout from "@/layout/dashboard/DashboardLayout.vue";
+import DashboardLayout from "../layout/dashboard/DashboardLayout.vue";
 // GeneralViews
-import NotFound from "@/pages/NotFoundPage.vue";
+import NotFound from "../pages/NotFoundPage.vue";
 
 // Admin pages
-import Dashboard from "@/pages/Dashboard.vue";
-import UserProfile from "@/pages/UserProfile.vue";
-import Notifications from "@/pages/Notifications.vue";
-import Icons from "@/pages/Icons.vue";
-import Maps from "@/pages/Maps.vue";
-import Typography from "@/pages/Typography.vue";
-import TableList from "@/pages/TableList.vue";
+import Dashboard from "../pages/Dashboard.vue";
+import UserProfile from "../pages/UserProfile.vue";
+import Notifications from "../pages/Notifications.vue";
+import Icons from "../pages/Icons.vue";
+import Maps from "../pages/Maps.vue";
+import Typography from "../pages/Typography.vue";
+import TableList from "../pages/TableList.vue";
 
 const routes = [
   {
