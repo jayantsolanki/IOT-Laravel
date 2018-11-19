3209ae83b62891a26e74309b36cdd62ed37d4cb4
Jayant Solanki <jayantjnp@gmail.com>
2018-11-19 05:45:18 -0500

    controller name

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'Api/UserController@login');
-Route::post('register', 'Api/UserController@register');
+Route::post('login', 'Api\UserController@login');
+Route::post('register', 'Api\UserController@register');
