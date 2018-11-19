914ed1b9a46baa09e6b747cd7f20980ad7974a72
Jayant Solanki <jayantjnp@gmail.com>
2018-11-19 02:50:03 -0500

    fixed landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,17 +2,29 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{csrf_token()}}">
-    <title>Treclon</title>
-    <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+    <title>Vue Paper Dashboard</title>
+    <!-- Bootstrap core CSS     -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <!--  Fonts and icons     -->
+    <link type="text/css" href="https://fonts.googleapis.com/css?family=Muli:400,300" rel="stylesheet">
+    <link type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="css/themify-icons.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <app></app>
-    </div>
-    <script src="{{ mix('js/bootstrap.js') }}"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
+<div class="wrapper" id="app">
+
+</div>
+
+<script src="{{ mix('/js/app.js') }}"></script>
+<!--  Google Maps Plugin    -->
+<script src="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyAamVCoyQ4AuvBpxVRMs9P-HFkfPVQj0Kw"
+        type="text/javascript"></script>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/{any}', 'IoTPageLander@index')->where('any', '.*');
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{any?}', 'IoTPageLander@index')->where('any', '^(?!(api|xyz).*$).*');
+
 
